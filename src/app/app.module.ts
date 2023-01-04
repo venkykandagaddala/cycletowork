c9e28f094e9a933cac8ea769b727f8cb87cb36e0
Venky <venkata@mapunity.in>
2023-01-04 17:30:36 +0530

    User, companies components and access the components uisng header links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { CompaniesComponent } from './companies/companies.component';
+
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: "users", component: UsersComponent},
+  { path: "companies", component: CompaniesComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    UsersComponent,
+    CompaniesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
